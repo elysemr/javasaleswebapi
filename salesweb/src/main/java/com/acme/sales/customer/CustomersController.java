eb61e6936c367e8496c31f8473d5ed13385e999f
Elyse Roth <elysemorganroth@gmail.com>
2021-10-25 12:01:31 -0400

    added get, put, post, delete, methods to db

--- salesweb/src/main/java/com/acme/sales/customer/CustomersController.java
+++ salesweb/src/main/java/com/acme/sales/customer/CustomersController.java
@@ -1,8 +1,15 @@
 package com.acme.sales.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,20 +22,57 @@ public class CustomersController {
 	private CustomerRepository custRepo; //composition
 	
 	@GetMapping
-	public Iterable<Customer> GetAll() {
-		return custRepo.findAll();
+	public ResponseEntity<Iterable<Customer>> GetAll() {
+		var customers = custRepo.findAll(); //will give us collection
+		return new ResponseEntity<Iterable<Customer>>(customers, HttpStatus.OK);
+			//^^passing back collection of customers and also error code so we can control what error code says
 	}
-	/*
+	
 	@GetMapping("{id}")
-	public Iterable<Customer> GetById() {
-		return custRepo.getById();
+	public ResponseEntity<Customer> GetById(@PathVariable int id) { //path variable: coming in from URL
+		var customer = custRepo.findById(id); //not an instance of customer, instance of optional that passes in customer
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND); //don't want to return null so response entity 
+																//points to real response with message
+		}
+		return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK); //.get is how you have to return
 	}
 	
-	@PutMapping("{/api/customers/}")
+	@PostMapping
+	public ResponseEntity<Customer> Insert(@RequestBody Customer customer) { //request body: path variable 
+														//but being passed in in body of request b/c it's a whole instance
+		if(customer == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST); //not required for capstone, but good practice IRL
+		}
+		customer.setId(0); //makes sure Id is set to 0
+		var newCustomer = custRepo.save(customer); //take entity which is customer instance
+		return new ResponseEntity<Customer>(newCustomer, HttpStatus.CREATED); //save returns customer so trap that in var and return that bc it has the correct id
+				//POST must return something, can't return no content
+	}
 	
-	@PostMapping("{/api/customers/}")
+	@PutMapping("{id}") //need URL because need to put in ID when updating
+	public ResponseEntity Update(@PathVariable int id, @RequestBody Customer customer) /*throws Exception */ //path var and data
+	{
+		if(customer.getId() != id) {
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		// throw new Exception("Ids do not match");
+		}
+		//could trap read by code to make sure no 2 customers have the same code
+		/* var updatedCustomer = */ 
+		custRepo.save(customer); //can choose not to store anything here bc not returning anything
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT); 
+	}
 	
-	@DeleteMapping("{/api/customers/}") */
+	@DeleteMapping("{id}") 
+	public ResponseEntity Delete(@PathVariable int id) {
+		var customer = custRepo.findById(id);
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		custRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		//customer.get();
+	}
 	
 	
 
--- salesweb/src/main/java/com/acme/sales/order/OrdersController.java
+++ salesweb/src/main/java/com/acme/sales/order/OrdersController.java
@@ -1,8 +1,15 @@
 package com.acme.sales.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +24,47 @@ public class OrdersController {
 	private OrderRepository ordRepo;
 	
 	@GetMapping
-	public Iterable<Order> GetAll() {
-		return ordRepo.findAll();
+	public ResponseEntity<Iterable<Order>> GetAll() {
+		var orders = ordRepo.findAll();
+		return new ResponseEntity<Iterable<Order>>(orders, HttpStatus.OK);
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity<Order> GetById(@PathVariable int id) {
+		var order = ordRepo.findById(id);
+		if(order.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Order>(order.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Order> Insert(@RequestBody Order order) {
+		if(order == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		order.setId(0);
+		var newOrder = ordRepo.save(order);
+		return new ResponseEntity<Order>(newOrder, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity Update(@PathVariable int id, @RequestBody Order order) {
+		if(order.getId() != id) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		ordRepo.save(order);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity Delete(@PathVariable int id) {
+		var order = ordRepo.findById(id);
+		if(order.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		ordRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
 }
